2207de75b3652278a7b85ff672d714c18c66c49b
Liz FIschenich <Liz.Devlin85@gmail.com>
2024-02-07 11:56:24 -0600

    refined models for restaurant and user

--- server/models/restaurant.js
+++ server/models/restaurant.js
@@ -3,5 +3,25 @@ const { Schema, model } = require("mongoose");
 const restaurantSchema = new Schema({
     restaurantName:{
         type: String,
+        required: true,
+    },
+    address:{
+        type: String,
+        required: true,
+    },
+    zip: {
+        type: String,
+        required: true,
+    },
+    location: {
+        type: {
+            type: String,
+            enum: ['Point'], 
+            default: 'Point'
+        },
+        coordinates: {
+            type: [Number], // Longitude, Latitude
+            required: true
+        }
     }
 })
